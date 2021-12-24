f4e9b8b8d074b107af62f0984ee8ec839817df6e
prestonfu <prestonmfu@gmail.com>
2021-12-23 17:56:41 -0800

    Initial commit

---
+++ sarscov2_lineage/data/countries_regions.py
@@ -0,0 +1,18 @@
+countries_regions = {
+  'Australia': 'Oceania',
+  'China': 'Asia',
+  'Hong Kong': 'Asia',
+  'Kazakhstan': 'Asia',
+  'India': 'Asia',
+  'Iran': 'Asia',
+  'Israel': 'Asia',
+  'Nepal': 'Asia',
+  'Pakistan': 'Asia',
+  'South Korea': 'Asia',
+  'Sri Lanka': 'Asia',
+  'Taiwan': 'Asia',
+  'Thailand': 'Asia',
+  'Turkey': 'Asia',
+  'USA': 'North America',
+  'Viet Nam': 'Asia'
+}
